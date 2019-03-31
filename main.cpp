5a50faf08e3fee13d761cab858fcc3255ea46e77
Brett Dong <brett.browning.dong@gmail.com>
2019-03-31 13:47:12 +0800

    Added command line parameter parser

--- main.cpp
+++ main.cpp
@@ -17,12 +17,23 @@
 #include <iostream>
 #include <cstdlib>
 #include <cstring>
+#include <string>
 #include <cstdio>
 #include <chrono>
+#include <signal.h>
 #include "constraint_set.h"
 #include "protocol.h"
 using namespace std;
 using namespace cv;
+
+void sig_handler(int sig)
+{
+    if(sig == SIGINT)
+    {
+        protocol::Disconnect();
+        exit(0);
+    }
+}
 
 void write(cv::Mat &img, const char *str, const cv::Point &pt)
 {
@@ -31,10 +42,26 @@ void write(cv::Mat &img, const char *str, const cv::Point &pt)
     cv::putText(img, str, pt, cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 255, 255), 1, 8);
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-    const char *serial_device = "/dev/ttyUSB0";
-    if(!protocol::Connect(serial_device))
+    int verbose = 0;
+    const char *default_serial_device = "/dev/ttyUSB0";
+    string serial_device(default_serial_device);
+    if(argc > 1)
+    {
+        for(int i = 1; i < argc; i++)
+        {
+            if(strcmp(argv[i], "--verbose") == 0 && i+1 < argc)
+            {
+                verbose = argv[++i][0] - '0';
+            }
+            else if(strcmp(argv[i], "--serial") == 0 && i+1 < argc)
+            {
+                serial_device = argv[++i];
+            }
+        }
+    }
+    if(!protocol::Connect(serial_device.c_str()))
     {
         cerr << "Unable to connect " << serial_device << endl;
         return 1;
@@ -55,6 +82,7 @@ int main()
     char buf[100];
     bool running = true;
     float angle_amp = 1.0f;
+    signal(SIGINT, sig_handler);
     while(running)
     {
         auto Tstart = chrono::system_clock::now();
@@ -62,41 +90,47 @@ int main()
             break;
         if(constraint_set::DetectArmor(img, target))
         {
-            sprintf(buf, "Detected @(% 6.2f, % 6.2f, % 6.2f)", target.x, target.y, target.z);
-            write(img, buf, cv::Point(10, 80));
             float yaw = atan2(target.x, target.z), pitch = atan2(target.y, sqrt(target.x*target.x + target.z*target.z));
             yaw = yaw / M_PI * 180; pitch = pitch / M_PI * 180;
-            sprintf(buf, "YAW=% 4.2fDEG PITCH=% 4.2fDEG", yaw, pitch);
-            write(img, buf, cv::Point(10, 100));
-            sprintf(buf, "AMP%fx YAW= % 4.2fDEG PITCH=% 4.2fDEG", angle_amp, yaw * angle_amp, pitch * angle_amp);
-            write(img, buf, cv::Point(10, 120));
             protocol::Send(yaw * angle_amp, pitch * angle_amp);
+            if(verbose > 0)
+            {
+                sprintf(buf, "Detected @(% 6.2f, % 6.2f, % 6.2f)", target.x, target.y, target.z);
+                write(img, buf, cv::Point(10, 80));
+                sprintf(buf, "YAW=% 4.2fDEG PITCH=% 4.2fDEG", yaw, pitch);
+                write(img, buf, cv::Point(10, 100));
+                sprintf(buf, "AMP%fx YAW= % 4.2fDEG PITCH=% 4.2fDEG", angle_amp, yaw * angle_amp, pitch * angle_amp);
+                write(img, buf, cv::Point(10, 120));
+            }
         }
-        auto Tend = chrono::system_clock::now();
-        auto duration = chrono::duration_cast<chrono::milliseconds>(Tend - Tstart);
-        const float latency = static_cast<float>(duration.count());// * chrono::milliseconds::period::num / chrono::milliseconds::period::den;
-        sprintf(buf, "INPUT: CAMERA %dx%d", img.cols, img.rows);
-        write(img, buf, cv::Point(5, 20));
-        sprintf(buf, "OUTPUT: %s", serial_device);
-        write(img, buf, cv::Point(5, 40));
-        sprintf(buf, "LATENCY: % 4.2f MS | %d FPS", latency, static_cast<int>(1000.0f / latency));
-        write(img, buf, cv::Point(5, 60));
-        line(img, Point(img.cols/2 - 10, img.rows/2), Point(img.cols/2 + 10, img.rows/2), Scalar(255, 0, 0));
-        line(img, Point(img.cols/2, img.rows/2 - 10), Point(img.cols/2, img.rows/2 + 10), Scalar(255, 0, 0));
-        imshow("constraint_set", img);
-        switch(waitKey(1))
+        if(verbose > 0)
         {
-            case 27:
-                running = false;
-                break;
-            case 'a':
-                if(angle_amp > 0.5f)
-                    angle_amp -= 0.5f;
-                break;
-            case 'd':
-                if(angle_amp < 9.5f)
-                    angle_amp += 0.5f;
-                break;
+            auto Tend = chrono::system_clock::now();
+            auto duration = chrono::duration_cast<chrono::milliseconds>(Tend - Tstart);
+            const float latency = static_cast<float>(duration.count());// * chrono::milliseconds::period::num / chrono::milliseconds::period::den;
+            sprintf(buf, "INPUT: CAMERA %dx%d", img.cols, img.rows);
+            write(img, buf, cv::Point(5, 20));
+            sprintf(buf, "OUTPUT: %s", serial_device.c_str());
+            write(img, buf, cv::Point(5, 40));
+            sprintf(buf, "LATENCY: % 4.2f MS | %d FPS", latency, static_cast<int>(1000.0f / latency));
+            write(img, buf, cv::Point(5, 60));
+            line(img, Point(img.cols/2 - 10, img.rows/2), Point(img.cols/2 + 10, img.rows/2), Scalar(255, 0, 0));
+            line(img, Point(img.cols/2, img.rows/2 - 10), Point(img.cols/2, img.rows/2 + 10), Scalar(255, 0, 0));
+            imshow("constraint_set", img);
+            switch(waitKey(1))
+            {
+                case 27:
+                    running = false;
+                    break;
+                case 'a':
+                    if(angle_amp > 0.5f)
+                        angle_amp -= 0.5f;
+                    break;
+                case 'd':
+                    if(angle_amp < 9.5f)
+                        angle_amp += 0.5f;
+                    break;
+            }
         }
     }
     protocol::Disconnect();
